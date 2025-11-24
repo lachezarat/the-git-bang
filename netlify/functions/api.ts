a9953ca5473df662f26f02bed9e9c6d1ffc71cf4
Lachezar <ehhno3333@gmail.com>
2025-11-24 22:40:44 +0200

    Fix: Include .env file in Netlify function bundle

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,9 @@
 import serverless from "serverless-http";
+import { config } from "dotenv";
+import { resolve } from "path";
+
+// Load .env file explicitly for Netlify Functions
+config({ path: resolve(__dirname, "../../.env") });
 
 import { createServer } from "../../server";
 
